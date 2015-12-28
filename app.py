f23b8cd552542909210683a19e187f12244c2401
roopesh90 <roopesh90nair@live.com>
2015-12-28 16:49:58 +0530

    db seeder added to app.py

--- app.py
+++ app.py
@@ -5,12 +5,38 @@ from tornado.options import options
 
 from settings import SETTINGS
 from urls import URL_PATTERNS
+import sqlite3
 
 class DiminuendoApp(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, URL_PATTERNS, **SETTINGS)
 
+def dbSeeder():
+    """Db seeder and verifier
+    """
+    connection = sqlite3.connect(SETTINGS['DBPATH'])
+    cursorobj = connection.cursor()
+    try:
+        cursorobj.execute('SELECT * FROM urlsbase')
+        print('Table already exists')
+    except:
+        print('Creating table \'urlsbase\'')
+        cursorobj.execute('CREATE TABLE urlsbase (\
+            id INTEGER PRIMARY KEY   AUTOINCREMENT,\
+            url text,\
+            title text,\
+            shrink text,\
+            hits text,\
+            created_at timestamp,\
+            updated_at timestamp)')
+        print('Successfully created table \'urlsbase\'')
+    connection.commit()
+    connection.close()
+    
 def main():
+    # Verify the database exists and has the correct layout
+    dbSeeder()
+    
     app = DiminuendoApp()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
