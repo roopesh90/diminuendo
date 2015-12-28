f7cffd5603d46f5fe584b18cff376346417c9f29
roopesh90 <roopesh90nair@live.com>
2015-12-28 20:24:40 +0530

    added title search handler url

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 import os
 import tornado.web
-from handlers import MainHandler, URLshrinkHandler, RedirectHandler
+from handlers import MainHandler, URLshrinkHandler, RedirectHandler, TitleSearchHandler
 
 PATH = lambda root, *a: os.path.join(root, *a)
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 URL_PATTERNS = [
     (r"/shrink/", URLshrinkHandler),
+    (r"/s/", TitleSearchHandler),
     (r"/", MainHandler),
     (r"/([a-zA-Z0-9\/]+)*", RedirectHandler),
 ]
