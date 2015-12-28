55c99c922a51d099aef9567e606add4fe65cb033
roopesh90 <roopesh90nair@live.com>
2015-12-28 17:00:41 +0530

    query executor added

--- handlers.py
+++ handlers.py
@@ -4,19 +4,31 @@ import tornado.web
 from base import BaseHandler
 import string
 import random
+import sqlite3
+from settings import SETTINGS as dummySettings
+
+def _execute(query):
+    connection = sqlite.connect(dummySettings['DBPATH'])
+    cursorobj = connection.cursor()
+    try:
+        cursorobj.execute(query)
+        result = cursorobj.fetchall()
+        connection.commit()
+    except Exception:
+        raise
+    connection.close()
+    return result
 
 class MainHandler(BaseHandler):
     def get(self):
         self.response["message"] = "Yo"
         self.write_json()
-
-
+    
 class URLshrinkHandler(BaseHandler):
     def post(self):
         url = self.get_json_argument('u')
         print(self.request.body)
-        print(a)
-        # self.response['hash'] = self.create_hash()
+        print(url)
         self.create_hash()
         self.write_json()
 
