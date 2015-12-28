8430a29ad29fbf6e2f89b55a79366b3d1fcd0ea9
roopesh90 <roopesh90nair@live.com>
2015-12-28 21:23:21 +0530

    Added - Meta url list url - Meta data url for short url

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 import os
 import tornado.web
-from handlers import MainHandler, URLshrinkHandler, RedirectHandler, TitleSearchHandler
+from handlers import MainHandler, URLshrinkHandler, RedirectHandler, TitleSearchHandler, URLMetaListHandler, URLMetaHandler
 
 PATH = lambda root, *a: os.path.join(root, *a)
 ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -8,6 +8,8 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 URL_PATTERNS = [
     (r"/shrink/", URLshrinkHandler),
     (r"/s/", TitleSearchHandler),
+    (r"/meta/", URLMetaListHandler),
+    (r"/meta/([a-zA-Z0-9\/]+)*", URLMetaHandler),
     (r"/", MainHandler),
     (r"/([a-zA-Z0-9\/]+)*", RedirectHandler),
 ]
